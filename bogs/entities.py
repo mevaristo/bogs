36fc5861bfc0e41a588d1934eaaa4310f5c0ec08
mevaristo <matheus.evaristo@protonmail.com>
2023-07-20 01:28:27 -0300

    Mapping entities

--- bogs/entities.py
+++ bogs/entities.py
@@ -1,4 +1,19 @@
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table
+from typing import List
+from typing import Tuple
+from datetime import date
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import MetaData
+from sqlalchemy import Date
+from sqlalchemy import Table
+from sqlalchemy import ForeignKey
+from geoalchemy2 import Geometry
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 DBAPI = 'psycopg2'
 USER = 'bogs'
@@ -9,14 +24,61 @@ DB = 'bogs_db'
 CONNECTION_URL = f'postgresql+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
 
 engine = create_engine(CONNECTION_URL)
-
 metadata = MetaData()
 
-cryptids = Table('cryptids', metadata,
-                 Column('id', Integer, primary_key=True),
-                 Column('name', String),
-                 Column('other_names', String),
-                 Column('description', String),
-                 Column('wiki', String))
+
+class Base(DeclarativeBase):
+    pass
+
+
+class Cryptid(Base):
+    __tablename__ = "cryptid"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+    other_names: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String)
+    wiki_link: Mapped[str] = mapped_column(String)
+    media: Mapped[List["Media"]] = relationship(
+        back_populates="cryptid", cascade="all"
+    )
+    sightings: Mapped[List["Sighting"]] = relationship(
+        back_populates="cryptid", cascade="all"
+    )
+
+
+class Media(Base):
+    __tablename__ = "media"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cryptid_id: Mapped[int] = mapped_column(ForeignKey("cryptid.id"))
+    cryptid: Mapped["Cryptid"] = relationship(
+        back_populates="media"
+    )
+    sighting_id: Mapped[int] = mapped_column(ForeignKey("sighting.id"))
+    sighting: Mapped["Sighting"] = relationship(
+        back_populates="media"
+    )
+
+    description: Mapped[str] = mapped_column(String)
+    link: Mapped[str] = mapped_column(String)
+    blob: Mapped[str] = mapped_column(String)
+
+
+class Sighting(Base):
+    __tablename__ = "sighting"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    cryptid_id: Mapped[int] = mapped_column(ForeignKey("cryptid.id"))
+    cryptid: Mapped["Cryptid"] = relationship(
+        back_populates="sightings"
+    )
+    media: Mapped[List["Media"]] = relationship(
+        back_populates="sighting", cascade="all"
+    )
+    description: Mapped[str] = mapped_column(String)
+    date: Mapped[date] = mapped_column(Date)
+    location: Mapped[Tuple[float, float]] = mapped_column(Geometry('POINT'))
+
 
 metadata.create_all(engine)
